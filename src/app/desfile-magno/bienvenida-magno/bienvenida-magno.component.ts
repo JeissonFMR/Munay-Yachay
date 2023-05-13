d5dc7bb16236fadabfdad7b30f2172f34850dc5e
Jeisson Montenegro <soyjeison2017@gmail.com>
2023-05-13 17:01:36 -0500

    Welcome desfile magno

--- src/app/desfile-magno/bienvenida-magno/bienvenida-magno.component.ts
+++ src/app/desfile-magno/bienvenida-magno/bienvenida-magno.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { TooltipPosition } from '@angular/material';
 
 @Component({
   selector: 'app-bienvenida-magno',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BienvenidaMagnoComponent implements OnInit {
 
+  imagenesSlider: Array<any> = [];
+
+  positionOptions: TooltipPosition[] = ['below', 'above', 'left', 'right'];
+  position = new FormControl(this.positionOptions[1]);
+
+  disfraz: string = `Ver categoria: Disfraz Individual`;
+  comparza: string = `Ver categoria: Comparza`;
+  murga: string = `Ver categoria: Murga`;
+  carroza: string = `Ver categoria: Carroza`;
+  carrozaNo: string = `Ver categoria: Carroza No Motorizada`;
+
+
   constructor() { }
 
   ngOnInit() {
+    this.imagenesSlider = [
+      {
+        imagen: 'https://via.placeholder.com/800x400?text=Imagen+2',
+        alt: 'Imagen 2'
+      },
+      {
+        imagen: 'https://via.placeholder.com/800x400?text=Imagen+3',
+        alt: 'Imagen 3'
+      },
+      {
+        imagen: 'https://via.placeholder.com/800x400?text=Imagen+4',
+        alt: 'Imagen 4'
+      },
+      {
+        imagen: 'https://via.placeholder.com/800x400?text=Imagen+5',
+        alt: 'Imagen 5'
+      }
+    ]
   }
 
 }
--- src/app/desfile-magno/desfile-magno.module.ts
+++ src/app/desfile-magno/desfile-magno.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { DesfileMagnoRoutingModule } from './desfile-magno-routing.module';
 import { BienvenidaMagnoComponent } from './bienvenida-magno/bienvenida-magno.component';
+import { MaterialModule } from 'app/materialAngular/material/material.module';
 
 @NgModule({
   imports: [
     CommonModule,
+    MaterialModule,
     DesfileMagnoRoutingModule
   ],
   declarations: [BienvenidaMagnoComponent]
